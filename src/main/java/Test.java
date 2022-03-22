71d06deda9bf8a4b6e13f860e58ee342183a3f40
nino0 <nino02022002@gmail.com>
2022-03-22 20:20:08 +0700

    ABC

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,6 +8,5 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         DanhSachHocVien danhSachHV = new DanhSachHocVien();
-        danhSachHV.nhapDiemHocVien();
     }
 }
